0efeb944ed0bf16ef1bc2670de2aa3cab8521f3a
Pablo-SanJose-Acedo <psanjosea@gmail.com>
2024-05-23 10:57:25 +0200

    two attempts of JS done. unsuccessful.

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 console.log('we are online')
 
-let step01 = "Tener los ingredientes de la masa: harina, sal, agua, aceite y levadura."
-let step02 = "Mezclar levadura con agua."
-let step03 = ""
-let step04 = ""
-let step05 = ""
-let step06 = ""
-let step07 = ""
-let step08 = ""
-let step09 = ""
-let step10 = ""
-let step11 = ""
-let step12 = ""
-let step13 = ""
-let step14 = ""
-let step15 = ""
+let steps = {
+"step01" = "Tener los ingredientes de la masa: harina, sal, agua, aceite y levadura."
+"step02" = "Mezclar levadura con agua."
+"step03" = "Añadir la harina y la sal a la mezcla."
+"step04" = "Añadir el agua y el aceite a la mezcla."
+"step05" = "Amasar hasta que quede una masa uniforme y no sea pegajosa."
+"step06" = "Dejar reposar la masa en un recipiente."
+"step07" = "Cuando la masa haya reposado el tiempo necesario, darle forma de disco."
+"step08" = "Pelar tomate, escaldarlo y triturarlo."
+"step09" = "Extender el tomate triturado sobre el disco de masa."
+"step10" = "Añadirle mozzarella recién rallada a la pizza."
+"step11" = "Añadirle taquitos de bacon a la pizza."
+"step12" = "Añadirle cebolla a la pizza."
+"step13" = "Precalentar el horno a 200 º C.".
+"step14" = "Cuando el horno esté listo, introducir la pizza en el horno."
+"step15" = "La pizza ha de estar 15 minutos en el horno."
+"step16" = "Extraer la pizza del horno."
+"step17" = "Añadir un poco de salsa barbacoa tipo cajún a temperatura ambiente sobre la pizza."
+"step18" = "Ya tienes tu pizza lista para servir y comer."
+}
+
+function imprimirSteps(steps) {
+    for (let [nombre, valor] of Object.entries(steps)) {
+        console.log(`${nombre}: ${valor}`);
+        console.log(`${nombre}: ${valor}`);
+        console.log(`${nombre}: ${valor}`);
+        console.log(`${nombre}: ${valor}`);
+        console.log(`${nombre}: ${valor}`);
+        console.log(`${nombre}: ${valor}`);
+        console.log(`${nombre}: ${valor}`);
+        
+        imprimirSteps(steps);/*
+
+        function imprimir() {
+            console.log(steps)
+        }
+        imprimir()
---
+++ app2.js
@@ -0,0 +1,6 @@
+let massRec ("agua" "sal" "harina" "aceite" "levadura")
+let toppingRec ("tomate" "queso" "bacon" "cebolla")
+let quantity ("5 gr" "10 gr" "20 gr" "50 gr" "100 gr" "150 gr")
+let temperature ("100 grados" "150 grados" "200 grados")
+let time ("5 minutos" "10 minutos" "15 minutos" "20 minutos" "25 minutos")
+
